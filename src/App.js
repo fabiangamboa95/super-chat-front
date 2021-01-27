d4c18a5fdbd57946f88002a951ab4090b3d9ef02
Fabian Gamboa <fabiangamboa95@gmail.com>
2021-01-27 14:34:13 -0500

    socket connected

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useEffect } from 'react';
 import './App.css';
+import openSocket from 'socket.io-client'
 
 function App() {
+
+  useEffect(() => {
+    const socket = openSocket('http://localhost:3001', {
+      withCredentials: true,
+      extraHeaders: {
+        'x-auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsImlhdCI6MTYxMTc3NDI5NSwiZXhwIjoxNjEyMzc5MDk1fQ.KXowULryeD-rOes8bHfNH7zqgWY0s8f-Tdas2P8Um3M'
+      }
+    })
+    socket.on('message', (message) => console.log(message))
+  }, [])
+
   return (
-    <div className="App">
-      what a cool library
+    <div className="app">
+      what a cool library!
     </div>
   );
 }
